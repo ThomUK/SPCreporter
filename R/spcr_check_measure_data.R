92ff91366e14a6acaf94199553d5488db59717ed
Tom Smith <thomas.smith5@nuh.nhs.uk>
2022-09-10 12:38:23 +0100

    add tests for checking measure data

--- R/spcr_check_measure_data.R
+++ R/spcr_check_measure_data.R
@@ -1,31 +1,21 @@
 #' (internal function) Check the measure data
 #'
-#' @param .data Dataframe. Data in wide-format
+#' @param .data List. List containing dataframes of data in wide format.
 #'
-#' @return Dataframe
+#' @return List
 #' @noRd
 #'
 spcr_check_measure_data <- function(.data) {
 
-  # pivot incoming measure_data from wide to long
-  long_data <- .data %>% tidyr::pivot_longer(
-    -c(.data$ref, .data$measure_name, .data$comment), names_to = "date", values_to = "value") %>%
-    dplyr::select(.data$ref, .data$measure_name, .data$date, .data$value) %>%
-    dplyr::filter(!is.na(.data$value))
-
-  # handle varying date column heading formats
-  suppressWarnings(
-    long_data <- long_data %>%
-      dplyr::mutate(
-        date = dplyr::case_when(
-
-          # when the date character string contains a "-"
-          grepl("-", date) == TRUE ~ as.Date(date, format = "%Y-%m-%d"),
-
-          # otherwise, its an excel character string that needs an origin
-          TRUE ~ as.Date(as.numeric(date),  origin = "1899-12-30")
-
-        )
-      )
+  assertthat::assert_that(
+    class(.data) == "list",
+    msg = "spcr_check_measure_data: The data must be a list."
   )
+
+  assertthat::assert_that(
+    all(names(.data) %in% c("Weekly", "Monthly")),
+    msg = "spcr_check_measure_data: The list items must be from 'Weekly' or 'Monthly'."
+  )
+
+  return(.data)
 }
