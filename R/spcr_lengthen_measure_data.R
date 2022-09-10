b7f8889202ec32782ff18c33c5278596d7e816da
Tom Smith <thomas.smith5@nuh.nhs.uk>
2022-09-10 12:38:56 +0100

    add function to lengthen measure data

---
+++ R/spcr_lengthen_measure_data.R
@@ -0,0 +1,37 @@
+#' (internal function) Transform the data from wide to long format
+#'
+#' @param .data Dataframe.  Data in wide format.
+#'
+#' @return Dataframe in long format
+#' @noRd
+#'
+spcr_lengthen_measure_data <- function(.data){
+
+  assertthat::assert_that(
+    "data.frame" %in% class(.data),
+    msg = "spcr_lengthen_measure_data: The data must be a dataframe."
+  )
+
+  # pivot incoming measure_data from wide to long
+  long_data <- .data %>% tidyr::pivot_longer(
+    -c(.data$ref, .data$measure_name, .data$comment), names_to = "date", values_to = "value") %>%
+    dplyr::select(.data$ref, .data$measure_name, .data$date, .data$value) %>%
+    dplyr::filter(!is.na(.data$value))
+
+  # handle varying date column heading formats
+  suppressWarnings(
+    long_data <- long_data %>%
+      dplyr::mutate(
+        date = dplyr::case_when(
+
+          # when the date character string contains a "-"
+          grepl("-", date) == TRUE ~ as.Date(date, format = "%Y-%m-%d"),
+
+          # otherwise, its an excel character string that needs an origin
+          TRUE ~ as.Date(as.numeric(date),  origin = "1899-12-30")
+
+        )
+      )
+  )
+
+}
