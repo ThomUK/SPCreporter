e7dde919a6fcb128e06fb635e1bc41736d212a31
Tom Smith <thomas.smith5@nuh.nhs.uk>
2022-10-20 22:52:15 +0100

    modify header bar

--- R/spcr_add_header_bar.R
+++ R/spcr_add_header_bar.R
@@ -8,12 +8,9 @@ spcr_add_header_bar <- function(){
   header_bar <- htmltools::div(
     htmltools::div(
       class = "inner_flex",
-      htmltools::div(spcr_mini_card("Updated to", "", class = "wide_card")),
-      htmltools::div(spcr_mini_card("Target", "")),
-      htmltools::div(spcr_mini_card("Set by", "")),
-      htmltools::div(spcr_mini_card("Actual", "")),
       htmltools::div("Vari -ation", class = "spc_logo"),
-      htmltools::div("Assur -ance", class = "spc_logo")
+      htmltools::div("Assur -ance", class = "spc_logo"),
+      htmltools::div("Data Quality", class = "spc_logo")
     ),
     class = "px-10"
   )
