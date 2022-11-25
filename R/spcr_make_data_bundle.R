cb224a27947f4ef2e63a48aba6669c82b21bff0e
Tom Smith <thomas.smith5@nuh.nhs.uk>
2022-11-25 22:59:47 +0000

    use base pipe

--- R/spcr_make_data_bundle.R
+++ R/spcr_make_data_bundle.R
@@ -13,15 +13,15 @@ spcr_make_data_bundle <- function(measure_data, report_config, measure_config) {
 
   # check measure_data, and lengthen the different aggregation levels into a single long dataframe
   # adding the frequency in as a column
-  measure_data <- spcr_check_measure_data(measure_data) %>%
+  measure_data <- spcr_check_measure_data(measure_data) |>
     purrr::map2_df(.y = names(measure_data), .f = spcr_lengthen_measure_data)
 
   # check measure_config
   measure_config <- spcr_check_measure_config(measure_config)
 
   # make a vector of the ref numbers to create charts for
-  refs <- report_config %>%
-    dplyr::pull("ref") %>%
+  refs <- report_config |>
+    dplyr::pull("ref") |>
     unique()
 
   # check reference numbers and measure names agree across both data frames
@@ -37,7 +37,7 @@ spcr_make_data_bundle <- function(measure_data, report_config, measure_config) {
     measure_data = measure_data,
     measure_config = measure_config,
     report_config = report_config
-  ) %>%
+  ) |>
     # add a column to control whether Domain titles are printed
     dplyr::mutate(
       Needs_Domain_Heading = dplyr::if_else(Domain != dplyr::lag(Domain, default = "TRUE"), TRUE, FALSE)
