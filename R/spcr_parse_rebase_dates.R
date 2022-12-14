25e7e962d8002fa20b11b55302cb9b0629953268
Fran Barton <francis.barton@nuh.nhs.uk>
2022-12-14 10:25:21 +0000

    Add spcr_align_rebase_dates() function and tests, so rebase changes are picked up by ptd

--- R/spcr_parse_rebase_dates.R
+++ R/spcr_parse_rebase_dates.R
@@ -27,3 +27,29 @@ spcr_parse_rebase_dates <- function(input) {
     )
   }
 }
+
+#' Align rebase date to match next data date after rebase, if does not already
+#' match a date from the relevant data.
+#' This is because plots were not showing rebase changes if the rebase date
+#' did not match a date in the data for that measure.
+#' https://github.com/ThomUK/SPCreporter/issues/35
+#'
+#' @inheritParams spcr_parse_rebase_dates
+#' @param measure_data data frame containing a column of date values
+#'
+#' @returns a vector of dates, amended as necessary, or NULL if no dates were present initially
+#' @noRd
+spcr_align_rebase_dates <- function(input, measure_data) {
+
+  dates <- spcr_parse_rebase_dates(input)
+
+  pull_closest_date <- function(date, dates_list) {
+    later_dates <- dates_list[dates_list >= date]
+    if (length(later_dates)) min(later_dates) else date
+  }
+
+  if (is.null(dates)) NULL
+  else dates |>
+    purrr::map_dbl(pull_closest_date, dates_list = measure_data$date) |>
+    lubridate::as_date()
+}
--- tests/testthat/test-spcr_parse_rebase_dates.R
+++ tests/testthat/test-spcr_parse_rebase_dates.R
@@ -26,3 +26,50 @@
     )
   })
 
+
+"dates are amended to match data dates" |>
+  test_that({
+
+    measure_data <- tibble::tibble(
+      date = lubridate::ymd(paste0("2022-0", 1:6, "-01"))
+    )
+
+    dates <- spcr_parse_rebase_dates('"2022-03-17", "2022-05-01", "2022-06-02"')
+
+    expect_length(dates, 3)
+
+    pull_closest_date <- function(date, dates_list) {
+      later_dates <- dates_list[dates_list >= date]
+      if (length(later_dates)) min(later_dates) else date
+    }
+
+    dates_list <- measure_data$date
+
+    later_dates <- dates_list[dates_list >= dates[1]]
+    expect_length(later_dates, 3)
+
+    later_dates <- dates_list[dates_list >= dates[2]]
+    expect_length(later_dates, 2)
+
+    later_dates <- dates_list[dates_list >= dates[3]]
+    expect_length(later_dates, 0)
+
+    expect_equal(pull_closest_date(dates[1], dates_list), as.Date("2022-04-01"))
+    expect_equal(pull_closest_date(dates[2], dates_list), as.Date("2022-05-01"))
+    expect_equal(pull_closest_date(dates[3], dates_list), as.Date("2022-06-02"))
+
+    out <- dates |>
+      # forthcoming versions of {purrr} should contain functions that enable
+      # output of a date vector directly (maybe called map_vec() or even
+      # map_date()) but for now we have to map to a character vector and then
+      # reformat. (purrr::map() followed by unlist() did not work, somehow.)
+      purrr::map_dbl(pull_closest_date, dates_list = dates_list) |>
+      lubridate::as_date()
+
+    expect_length(out, 3)
+
+    expect_s3_class(out[1], "Date")
+    expect_s3_class(out[2], "Date")
+    expect_s3_class(out[3], "Date")
+
+  })
