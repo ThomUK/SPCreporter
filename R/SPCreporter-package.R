c7b19e029a5a3c83ac041c2a4f97835cf0a41307
Fran Barton <francis.barton@nuh.nhs.uk>
2023-06-22 00:04:09 +0100

    Update some package documentation

--- R/SPCreporter-package.R
+++ R/SPCreporter-package.R
@@ -2,7 +2,9 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom dplyr across last_col
+#' @importFrom dplyr across if_any pick
+#' @importFrom rlang := .data
+#' @importFrom tidyselect all_of any_of everything
 #' @importFrom utils head tail
 ## usethis namespace: end
 NULL
